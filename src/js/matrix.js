c88e15dd097b73f2bc5ea431b1dc4e02f65e3e15
KHA TIEU <kha.tieu@student.lab.fi>
2023-09-26 22:35:47 +0300

    1st commit

---
+++ src/js/matrix.js
@@ -0,0 +1,24 @@
+const Matrix = {
+    moveUp : () => {
+        console.log('move up')
+    },
+    moveDown : () => {
+        console.log('move down')
+    },
+    moveLeft : () => {
+        console.log('move left')
+    },
+    moveRight : () => {
+        console.log('move right')
+    },
+    rotateLeft : () => {
+        console.log('rotate left')
+    },
+    rotateRight : () => {
+        console.log('rotate right')
+    },
+    create : () => {
+        console.log('create')
+    }
+}
+module.exports = Matrix
